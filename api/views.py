6b409fec73cf0c905e57172b5149266ddd0855cf
unknown <edwinrmrjr@gmail.com>
2024-09-06 09:11:25 +0800

    views

--- api/views.py
+++ api/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import BlogPost
+from .serializers import BlogPostSerializer
 
 # Create your views here.
+class BlogPostListCreate(generics.ListCreateView):
+    queryset = BlogPost.objects.all()
+    serializer_class = BlogPostSerializer
